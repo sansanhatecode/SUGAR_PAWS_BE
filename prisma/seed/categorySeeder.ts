5e1912c039d9d2d11eba5a8e5c1928270b18d08f
Linh Nguyễn Kiều (UPP DEL) <linhnk@upp-technology.com>
2025-03-14 18:32:18 +0700

    rfaksfjkasdjf

--- prisma/seed/categorySeeder.ts
+++ prisma/seed/categorySeeder.ts
@@ -3,27 +3,72 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function seedCategories() {
-  const lolitaDresses = await prisma.category.create({
-    data: { name: 'lolita dresses' },
-  });
+  const categoriesData = [
+    {
+      name: 'ABOUT',
+      subcategories: [
+        'about us',
+        'retail location',
+        "faq's",
+        'event calendar',
+        'event booking',
+      ],
+    },
+    {
+      name: 'ACCESSORIES',
+      subcategories: [
+        'gloves',
+        'harnesses and belts',
+        'lashes',
+        'neckwear',
+        'parasols',
+        'wristcuffs',
+        'bags',
+        'pins and patches',
+        'shoes',
+      ],
+    },
+    {
+      name: 'HAIR',
+      subcategories: ['bows', 'hair claws', 'scrunchies', 'star clips', 'hats'],
+    },
+    {
+      name: 'CLOTHING',
+      subcategories: [
+        'blouses',
+        'jumperskirts',
+        'onepieces',
+        'skirts',
+        'pants',
+      ],
+    },
+    {
+      name: 'JEWELRY',
+      subcategories: ['bracelets', 'earrings', 'flairs', 'necklaces', 'rings'],
+    },
+    {
+      name: 'PLUS SIZE',
+      subcategories: [
+        'plus size dresses',
+        'plus size skirts',
+        'plus size blouses',
+        'plus size petticoat & bloomers',
+      ],
+    },
+  ];
 
-  const accessories = await prisma.category.create({
-    data: { name: 'accessories' },
-  });
+  for (const category of categoriesData) {
+    const parent = await prisma.category.create({
+      data: { name: category.name },
+    });
 
-  const shoes = await prisma.category.create({
-    data: { name: 'shoes' },
-  });
+    await prisma.category.createMany({
+      data: category.subcategories.map((sub) => ({
+        name: sub,
+        parentId: parent.id,
+      })),
+    });
+  }
 
-  await prisma.category.createMany({
-    data: [
-      { name: 'one piece', parentId: lolitaDresses.id },
-      { name: 'jumperskirt', parentId: lolitaDresses.id },
-      { name: 'blouse', parentId: lolitaDresses.id },
-      { name: 'pannier', parentId: accessories.id },
-      { name: 'headwear', parentId: accessories.id },
-      { name: 'boots', parentId: shoes.id },
-      { name: 'mary janes', parentId: shoes.id },
-    ],
-  });
+  console.log('Categories seeded successfully!');
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,9 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { MailModule } from './mail/mail.module';
-import { CacheManagerModule } from './cache/cache.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { redisStore } from 'cache-manager-ioredis-yet';
 
 @Module({
   imports: [
@@ -11,7 +13,20 @@ import { CacheManagerModule } from './cache/cache.module';
     AuthModule,
     ProductModule,
     MailModule,
-    CacheManagerModule,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: await redisStore({
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+          host: configService.get('REDIS_HOST') ?? 'localhost',
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+          port: configService.get<number>('REDIS_PORT') ?? 6379,
+          ttl: 300, // Thời gian sống của cache (giây)
+        }),
+      }),
+      isGlobal: true,
+    }),
   ],
 })
 export class AppModule {}
